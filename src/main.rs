f666ee7568349b4a703ee3f93d9cdf9307a481b5
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-02-24 22:41:33 +1030

    Add camera definition and anti-aliasing via sampling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod colour;
 mod common;
 mod hittable;
@@ -8,12 +9,13 @@ mod vec3;
 
 use std::io;
 
+use camera::Camera;
 use colour::Colour;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
 use ray::Ray;
 use sphere::Sphere;
-use vec3::{Point3, Vec3};
+use vec3::Point3;
 
 fn ray_colour(r: &Ray, world: &dyn Hittable) -> Colour {
     let mut rec = HitRecord::new();
@@ -31,6 +33,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+    const SAMPLES_PER_PIXEL: i32 = 100;
 
     // World
     let mut world = HittableList::new();
@@ -38,15 +41,7 @@ fn main() {
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     // Render
     println!("P3");
@@ -54,16 +49,16 @@ fn main() {
     println!("255");
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {j: >5}");
         for i in 0..IMAGE_WIDTH {
-            eprint!("\rScanlines remaining: {}", j);
-            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_colour = ray_colour(&r, &world);
-            colour::write_colour(&mut io::stdout(), pixel_colour);
+            let mut pixel_colour = Colour::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + common::random_double()) / (IMAGE_WIDTH - 1) as f64;
+                let v = (j as f64 + common::random_double()) / (IMAGE_HEIGHT - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(&r, &world);
+            }
+            colour::write_colour(&mut io::stdout(), pixel_colour, SAMPLES_PER_PIXEL);
         }
     }
 
