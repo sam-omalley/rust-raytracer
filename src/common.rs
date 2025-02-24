d55bc820859fb16857c32a7df0ae2553cbb2d883
Samuel O'Malley <git@sam.omalley.cc>
2025-02-24 22:41:33 +1030

    Add camera definition and anti-aliasing via sampling

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,31 @@
+use rand::Rng;
+
 // Constants
 
-pub use std::f64::consts::PI;
 pub use std::f64::INFINITY;
+pub use std::f64::consts::PI;
 
 // Utility functions
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
+}
+
+pub fn random_double() -> f64 {
+    rand::rng().random()
+}
+
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+    // Return a random real in [min, max)
+    min + (max - min) * random_double()
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+    x
 }
