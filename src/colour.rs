95845b11455fc2d5c2a1cbcbc54ff0157b998124
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-02-24 22:16:38 +1030

    Make hittable sphere more generic

--- src/colour.rs
+++ src/colour.rs
@@ -2,9 +2,9 @@ use std::io::Write;
 
 use crate::vec3::Vec3;
 
-pub type Color = Vec3;
+pub type Colour = Vec3;
 
-pub fn write_colour(out: &mut impl Write, pixel_color: Color) {
+pub fn write_colour(out: &mut impl Write, pixel_color: Colour) {
     // Write the translated [0, 255] value of each colour component.
     let r = (255.999 * pixel_color.x()) as i32;
     let g = (255.999 * pixel_color.y()) as i32;
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
 mod colour;
+mod common;
+mod hittable;
+mod hittable_list;
 mod ray;
+mod sphere;
 mod vec3;
 
 use std::io;
 
-use colour::Color;
+use colour::Colour;
+use hittable::{HitRecord, Hittable};
+use hittable_list::HittableList;
 use ray::Ray;
+use sphere::Sphere;
 use vec3::{Point3, Vec3};
 
-fn hit_sphere(centre: Point3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin() - centre;
-    let a = r.direction().length_squared();
-    let half_b = vec3::dot(oc, r.direction());
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - f64::sqrt(discriminant)) / a
+fn ray_colour(r: &Ray, world: &dyn Hittable) -> Colour {
+    let mut rec = HitRecord::new();
+    if world.hit(r, 0.0, common::INFINITY, &mut rec) {
+        return 0.5 * (rec.normal + Colour::new(1.0, 1.0, 1.0));
     }
-}
 
-fn ray_colour(r: &Ray) -> Color {
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
-
-    if t > 0.0 {
-        let n = vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
-        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
-    }
     let unit_direction = vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    (1.0 - t) * Colour::new(1.0, 1.0, 1.0) + t * Colour::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
@@ -39,6 +31,11 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+    // World
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
     let viewport_height = 2.0;
@@ -65,7 +62,7 @@ fn main() {
                 origin,
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
-            let pixel_colour = ray_colour(&r);
+            let pixel_colour = ray_colour(&r, &world);
             colour::write_colour(&mut io::stdout(), pixel_colour);
         }
     }
