542caf15d88c75d55b01ba541d508bb3d6128b60
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-02-26 22:45:54 +1030

    Add Metal material

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,17 +1,21 @@
 use crate::hittable::{HitRecord, Hittable};
+use crate::material::Material;
 use crate::ray::Ray;
 use crate::vec3::{self, Point3};
+use std::rc::Rc;
 
 pub struct Sphere {
     centre: Point3,
     radius: f64,
+    mat: Rc<dyn Material>,
 }
 
 impl Sphere {
-    pub fn new(cen: Point3, r: f64) -> Sphere {
+    pub fn new(cen: Point3, r: f64, m: Rc<dyn Material>) -> Sphere {
         Sphere {
             centre: cen,
             radius: r,
+            mat: m,
         }
     }
 }
@@ -42,6 +46,7 @@ impl Hittable for Sphere {
         rec.p = r.at(rec.t);
         let outward_normal = (rec.p - self.centre) / self.radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = Some(self.mat.clone());
         true
     }
 }
