c10c2f6c095b6d3f715696671e6c02f9048608f9
Sam O'Malley <1546488+sam-omalley@users.noreply.github.com>
2025-02-27 21:17:41 +1030

    Position and orient the camera using geometry

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ use camera::Camera;
 use colour::Colour;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
-use material::Lambertian;
+use material::{Dialectric, Lambertian, Metal};
 use ray::Ray;
 use sphere::Sphere;
-use vec3::Point3;
+use vec3::{Point3, Vec3};
 
 fn ray_colour(r: &Ray, world: &dyn Hittable, depth: i32) -> Colour {
     if depth <= 0 {
@@ -52,26 +52,49 @@ fn main() {
     const SAMPLES_PER_PIXEL: i32 = 100;
     const MAX_DEPTH: i32 = 50;
 
+    // Camera
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        ASPECT_RATIO,
+    );
+
     // World
-    let r = f64::cos(common::PI / 4.0);
     let mut world = HittableList::new();
 
-    let material_left = Rc::new(Lambertian::new(Colour::new(0.0, 0.0, 1.0)));
-    let material_right = Rc::new(Lambertian::new(Colour::new(1.0, 0.0, 0.0)));
+    let material_ground = Rc::new(Lambertian::new(Colour::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Colour::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dialectric::new(1.5));
+    let material_bubble = Rc::new(Dialectric::new(1.0 / 1.5));
+    let material_right = Rc::new(Metal::new(Colour::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        material_center,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         material_left,
     )));
     world.add(Box::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        material_bubble,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         material_right,
     )));
-
-    // Camera
-    let cam = Camera::new(90.0, ASPECT_RATIO);
 
     // Render
     println!("P3");
