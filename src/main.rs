dabbd95800a7ba9856cd5368629c92c2023c50bc
Samuel O'Malley <git@sam.omalley.cc>
2025-02-27 21:10:29 +1030

    Add fov to camera

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use camera::Camera;
 use colour::Colour;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
-use material::{Dialectric, Lambertian, Metal};
+use material::Lambertian;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Point3;
@@ -53,42 +53,25 @@ fn main() {
     const MAX_DEPTH: i32 = 50;
 
     // World
+    let r = f64::cos(common::PI / 4.0);
     let mut world = HittableList::new();
 
-    let material_ground = Rc::new(Lambertian::new(Colour::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Colour::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dialectric::new(1.5));
-    let material_bubble = Rc::new(Dialectric::new(1.0 / 1.5));
-    let material_right = Rc::new(Metal::new(Colour::new(0.8, 0.6, 0.2), 0.0));
+    let material_left = Rc::new(Lambertian::new(Colour::new(0.0, 0.0, 1.0)));
+    let material_right = Rc::new(Lambertian::new(Colour::new(1.0, 0.0, 0.0)));
 
     world.add(Box::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
+        Point3::new(-r, 0.0, -1.0),
+        r,
         material_left,
     )));
     world.add(Box::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
+        Point3::new(r, 0.0, -1.0),
+        r,
         material_right,
     )));
 
     // Camera
-    let cam = Camera::new();
+    let cam = Camera::new(90.0, ASPECT_RATIO);
 
     // Render
     println!("P3");
