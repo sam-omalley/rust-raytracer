4248404473cebc936679d94218569296f2680343
Samuel O'Malley <git@sam.omalley.cc>
2025-02-28 00:05:59 +1030

    Use rayon for parallel processing

--- src/colour.rs
+++ src/colour.rs
@@ -1,11 +1,9 @@
-use std::io::Write;
-
 use crate::common;
 use crate::vec3::Vec3;
 
 pub type Colour = Vec3;
 
-pub fn write_colour(out: &mut impl Write, pixel_colour: Colour, samples_per_pixel: i32) {
+pub fn write_colour(pixel_colour: Colour, samples_per_pixel: i32) -> String {
     let mut r = pixel_colour.x();
     let mut g = pixel_colour.y();
     let mut b = pixel_colour.z();
@@ -17,12 +15,10 @@ pub fn write_colour(out: &mut impl Write, pixel_colour: Colour, samples_per_pixe
     b = f64::sqrt(scale * b);
 
     // Write the translated [0, 255] value of each colour component.
-    writeln!(
-        out,
+    format!(
         "{} {} {}",
         (256.0 * common::clamp(r, 0.0, 0.999)) as i32,
         (256.0 * common::clamp(g, 0.0, 0.999)) as i32,
         (256.0 * common::clamp(b, 0.0, 0.999)) as i32,
     )
-    .expect("Writing colour");
 }
--- src/material.rs
+++ src/material.rs
@@ -3,125 +3,65 @@ use crate::hittable::HitRecord;
 use crate::ray::Ray;
 use crate::{common, vec3};
 
-pub trait Material {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Colour,
-        scatter: &mut Ray,
-    ) -> bool;
+pub enum Material {
+    Lambertian { albedo: Colour },
+    Metal { albedo: Colour, fuzziness: f64 },
+    Dialectric { refraction: f64 },
 }
 
-pub struct Lambertian {
-    albedo: Colour,
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    // Use Schlick's approximation for reflectance
+    let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 = r0 * r0;
+    r0 + (1.0 - r0) * f64::powf(1.0 - cosine, 5.0)
 }
 
-impl Lambertian {
-    pub fn new(a: Colour) -> Lambertian {
-        Lambertian { albedo: a }
-    }
-}
+impl Material {
+    pub fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Colour, Ray)> {
+        match self {
+            Material::Lambertian { albedo } => {
+                let mut scatter_direction = rec.normal + vec3::random_unit_vector();
 
-impl Material for Lambertian {
-    fn scatter(
-        &self,
-        _r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Colour,
-        scattered: &mut Ray,
-    ) -> bool {
-        let mut scatter_direction = rec.normal + vec3::random_unit_vector();
+                // Catch degenerate scatter direction
+                if scatter_direction.near_zero() {
+                    scatter_direction = rec.normal;
+                }
 
-        // Catch degenerate scatter direction
-        if scatter_direction.near_zero() {
-            scatter_direction = rec.normal;
+                Some((*albedo, Ray::new(rec.p, scatter_direction)))
+            }
+            Material::Metal { albedo, fuzziness } => {
+                let reflected = vec3::reflect(vec3::unit_vector(r_in.direction()), rec.normal);
+                let scattered =
+                    Ray::new(rec.p, reflected + *fuzziness * vec3::random_in_unit_sphere());
+
+                if vec3::dot(scattered.direction(), rec.normal) <= 0.0 {
+                    None
+                } else {
+                    Some((*albedo, scattered))
+                }
+            }
+            Material::Dialectric { refraction } => {
+                let refraction_ratio = if rec.front_face {
+                    1.0 / *refraction
+                } else {
+                    *refraction
+                };
+
+                let unit_direction = vec3::unit_vector(r_in.direction());
+                let cos_theta = f64::min(vec3::dot(-unit_direction, rec.normal), 1.0);
+                let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
+
+                let cannot_refract = refraction_ratio * sin_theta > 1.0;
+                let direction = if cannot_refract
+                    || reflectance(cos_theta, refraction_ratio) > common::random_double()
+                {
+                    vec3::reflect(unit_direction, rec.normal)
+                } else {
+                    vec3::refract(unit_direction, rec.normal, refraction_ratio)
+                };
+
+                Some((Colour::new(1.0, 1.0, 1.0), Ray::new(rec.p, direction)))
+            }
         }
-
-        *attenuation = self.albedo;
-        *scattered = Ray::new(rec.p, scatter_direction);
-        true
-    }
-}
-
-pub struct Metal {
-    albedo: Colour,
-    fuzz: f64,
-}
-
-impl Metal {
-    pub fn new(a: Colour, f: f64) -> Metal {
-        Metal {
-            albedo: a,
-            fuzz: if f < 1.0 { f } else { 1.0 },
-        }
-    }
-}
-
-impl Material for Metal {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Colour,
-        scattered: &mut Ray,
-    ) -> bool {
-        let reflected = vec3::reflect(vec3::unit_vector(r_in.direction()), rec.normal);
-
-        *attenuation = self.albedo;
-        *scattered = Ray::new(rec.p, reflected + self.fuzz * vec3::random_in_unit_sphere());
-        vec3::dot(scattered.direction(), rec.normal) > 0.0
-    }
-}
-
-pub struct Dialectric {
-    ir: f64, // Index of refraction
-}
-
-impl Dialectric {
-    pub fn new(index_of_refraction: f64) -> Dialectric {
-        Dialectric {
-            ir: index_of_refraction,
-        }
-    }
-
-    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
-        // Use Schlick's approximation for reflectance
-        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
-        r0 = r0 * r0;
-        r0 + (1.0 - r0) * f64::powf(1.0 - cosine, 5.0)
-    }
-}
-
-impl Material for Dialectric {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Colour,
-        scattered: &mut Ray,
-    ) -> bool {
-        let refraction_ratio = if rec.front_face {
-            1.0 / self.ir
-        } else {
-            self.ir
-        };
-
-        let unit_direction = vec3::unit_vector(r_in.direction());
-        let cos_theta = f64::min(vec3::dot(-unit_direction, rec.normal), 1.0);
-        let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
-
-        let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract
-            || Self::reflectance(cos_theta, refraction_ratio) > common::random_double()
-        {
-            vec3::reflect(unit_direction, rec.normal)
-        } else {
-            vec3::refract(unit_direction, rec.normal, refraction_ratio)
-        };
-
-        *attenuation = Colour::new(1.0, 1.0, 1.0);
-        *scattered = Ray::new(rec.p, direction);
-        true
     }
 }
