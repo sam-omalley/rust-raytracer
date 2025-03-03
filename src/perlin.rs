8cda28a77b0233cac42b0ebf8c60b9d6d3c83d12
Samuel O'Malley <git@sam.omalley.cc>
2025-03-03 21:29:24 +1030

    Smooth perlin noise with trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -32,14 +32,28 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = ((4.0 * p.x()) as i32) & 255;
-        let j = ((4.0 * p.y()) as i32) & 255;
-        let k = ((4.0 * p.z()) as i32) & 255;
+        let u = p.x() - f64::floor(p.x());
+        let v = p.y() - f64::floor(p.y());
+        let w = p.z() - f64::floor(p.z());
 
-        let index: i32 =
-            self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize];
+        let i = f64::floor(p.x()) as i32;
+        let j = f64::floor(p.y()) as i32;
+        let k = f64::floor(p.z()) as i32;
 
-        self.randfloat[index as usize]
+        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let index = self.perm_x[((i + di) & 255) as usize]
+                        ^ self.perm_y[((j + dj) & 255) as usize]
+                        ^ self.perm_z[((k + dk) & 255) as usize];
+                    c[di as usize][dj as usize][dk as usize] = self.randfloat[index as usize];
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
     }
 
     fn perlin_generate_perm(p: &mut [i32; POINT_COUNT]) {
@@ -58,6 +72,22 @@ impl Perlin {
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    #[allow(clippy::needless_range_loop)]
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
 
